d97699236746dcfaf7d745ac11dd0a31ded1d986
Shawn <myking123321@gmail.com>
2025-12-12 13:00:31 +0700

    wip:model2-ollama

--- main.py
+++ main.py
@@ -11,24 +11,43 @@ load_dotenv()
 
 # #Inisialisasi Objek dan Variabel
 dataset = ds.DatasetAgent()
-model= 'gpt-4o-mini'
+
+config_list = [{
+    
+}]
+# model= 'gpt-4o-mini'
+# llm_config= {
+#     "model": model,
+#     "api_key" : os.environ.get("OPENAI_API_KEY"),
+#     "temperature":0,
+#     "functions" : [schema.dataset_schema],
+#     "function_call" : "auto"
+# }
+
+config_list = [{
+    "model" : os.environ.get("LOCAL_LLAMA_MODEL"),
+    "base_url": os.environ.get("LOCAL_LLAMA_URL"),
+    "api_base": os.environ.get("LOCAL_LLAMA_URL"),
+    "api_key" : "ollama",
+}]
 llm_config= {
-    "model": model,
-    "api_key" : os.environ.get("OPENAI_API_KEY"),
+    "config_list" : config_list,
+    # "mode;"
     "temperature":0,
+    "cache_seed":42,
     "functions" : [schema.dataset_schema],
-    "function_call" : "auto"
+    # "function_call" : "auto"
 }
 
 # # UserProxyAgent
 user = UserProxyAgent(
     name='executor',
-    human_input_mode='ALWAYS',
+    human_input_mode='TERMINATE',
+    max_consecutive_auto_reply=2,
     code_execution_config={
         "work_dir": "user_representation",
         "use_docker": False
     },
-    function_map={"filter_data": dataset.filter_data}
 )
 
 # user
@@ -56,6 +75,7 @@ profile_interpreter = AssistantAgent(
 
 
     """,
+    max_consecutive_auto_reply=2,
     llm_config=llm_config
 )
 
@@ -63,11 +83,12 @@ profile_interpreter = AssistantAgent(
 program_data = UserProxyAgent(
     name="program_data",
     human_input_mode="NEVER",
-    max_consecutive_auto_reply=5,
+    max_consecutive_auto_reply=2,
     code_execution_config=False,
     function_map={
         "filter_data": dataset.filter_data
     }
+    
 )
 
 #ProgramFitEvaluatorAgent (LLM)
@@ -83,6 +104,7 @@ program_fit_evaluator = AssistantAgent(
         - "Ambitious Target": Program yang bagus tapi perlu peningkatan atau penyesuaian lebih lanjut dari profil
         
         Berikan skor dan penjelasan alasan untuk setiap program""",
+        max_consecutive_auto_reply=2,
     llm_config=llm_config
 )
 
@@ -101,8 +123,20 @@ study_recommendation = AssistantAgent(
             - Persyaratan yang perlu diperhatikan
         4. Berikan saran untuk penguatan profil jika pengguna ingin mengejar pilihan yang lebih ambisius
     """,
+    max_consecutive_auto_reply=2,
     llm_config=llm_config
 )
+
+profile_interpreter.register_function(
+    function_map={
+        "filter_data": dataset.filter_data
+    }
+)
+# profile_interpreter.register_function(
+#     function_map={
+#         "filter_data": dataset.filter_data
+#     }
+# )
 
 # records = dataset.filter_data(minat_bidang="engineering")
 # if records is not None:
@@ -117,10 +151,16 @@ if __name__ == "__main__":
     #     for r in records[:3]:
     #         print(r)
             
-    groupchat = GroupChat(agents=[user,profile_interpreter,program_data,program_fit_evaluator,study_recommendation]
-                       ,messages=[] )
-    groupchat_mgr = GroupChatManager(groupchat, 
-                 llm_config=llm_config)
+    groupchat = GroupChat(
+        agents=[user,profile_interpreter,program_data,program_fit_evaluator,study_recommendation],
+        messages=[],
+        max_round=5
+        )
+    groupchat_mgr = GroupChatManager(
+        groupchat, 
+        llm_config=llm_config,
+        max_consecutive_auto_reply=10,
+        human_input_mode="ALWAYS")
     
     user.initiate_chat(
         groupchat_mgr,
